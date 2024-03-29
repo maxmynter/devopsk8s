9a23f74740dd05829a1155eca7d1f9fdcdce61d1
Max Mynter <maxmynter@me.com>
2024-03-30 00:41:39 +0100

    ex1.05

---
+++ ex1.05/project_v0.3/main.js
@@ -0,0 +1,27 @@
+const http = require('node:http');
+const fs = require('fs')
+
+const hostname = '127.0.0.1';
+const port = process.env.PORT;
+
+const server = http.createServer((req, res) => {
+	if (req.method == 'GET' && req.url == '/') {
+		fs.readFile('index.html', (err, data) => {
+			if (err) {
+				res.statusCode = 500;
+				res.setHeader('Content-Type', 'text/plain');
+				res.end('Error reading file.')
+			} else {
+				res.statusCode = 200;
+				res.setHeader('Content-Type', 'text/html');
+				res.end(data);
+			}
+		});
+	} else {
+		res.statusCode = 404;
+		res.setHeader('Content-Type', 'text/error');
+		res.end('Not Found\n');
+	}
+});
+
+server.listen(port, hostname, () => { console.log(`Server running at http://${hostname}:${port}/`); });
