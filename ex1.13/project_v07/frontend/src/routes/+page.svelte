d9d7c9f945d8d94b49aa80c85a9f9a8a01ba3ed4
Max Mynter <maxmynter@me.com>
2024-04-10 12:06:49 +0200

    ex1.13

--- ex1.13/project_v07/frontend/src/routes/+page.svelte
+++ ex1.13/project_v07/frontend/src/routes/+page.svelte
@@ -1,5 +1,24 @@
 <script>
 	let imageURL = 'http://localhost:3005/image';
+	let newTodo = '';
+	let todos = [
+		// Hardcoded list of todos
+		{ id: 1, text: 'Learn Svelte basics' },
+		{ id: 2, text: 'Build a Svelte project' },
+		{ id: 3, text: 'Explore SvelteKit for SSR' },
+		{ id: 4, text: 'Learn K8s' }
+	];
+
+	function addTodo() {
+		if (newTodo.trim() !== '' && newTodo.length <= 140) {
+			const todo = {
+				id: todos.length + 1,
+				text: newTodo
+			};
+			todos = [...todos, todo];
+			newTodo = '';
+		}
+	}
 </script>
 
 <h1>DevOps with K8s Project</h1>
@@ -8,3 +27,30 @@
 {:else}
 	<p>Loading Image...</p>
 {/if}
+
+<div class="todo-input">
+	<input type="text" bind:value={newTodo} placeholder="Add new Todo" maxlength="140" />
+	<button on:click={addTodo}>Send</button>
+</div>
+
+<ul class="todos-list">
+	{#each todos as { id, text }}
+		<li>{text}</li>
+	{/each}
+</ul>
+
+<style>
+	.todo-input {
+		margin-top: 20px;
+	}
+	.todos-list {
+		margin-top: 10px;
+		list-style-type: none;
+		padding: 0;
+	}
+	.todos-list li {
+		padding: 8px;
+		margin-bottom: 4px;
+		background-color: #f3f3f3;
+	}
+</style>
